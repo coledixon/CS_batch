1d192f09a773241ab2d8501e664cfe58a026d859
colefromportland <colefromportland@gmail.com>
2019-01-25 12:03:09 -0700

    bat file for compiling single .cs project
    compile basic C# cmd line application through .bat file.
    
    run as .exe

---
+++ CS_drill_compile/README.md
@@ -0,0 +1,16 @@
+# CS_drill_compile
+
+-- simple batch file for reading a .txt file containing cs code, and compiling the script into CLI .exe
+
+
+### INSTRUCTIONS:
+
+-- place code from program.cs into cs.txt (Ctrl+C, Ctrl+V)
+
+-- run cs_drill_compile.bat
+
+-- if ERRORLEVEL 1 during compile .exe || run .exe, cmd will output error message
+
+-- cmd line application (drill) will run if ERRORLEVEL 0
+
+-- cmd line closes after application complete
---
+++ CS_drill_compile/cs_drill_compile.bat
@@ -0,0 +1,50 @@
+@echo off
+rem BUILT BY: COLE DIXON 2018
+title compile single .cs drill
+
+echo CLEAR EXISTING OBJECT(S) FROM ROOT
+rem delete .exe if exists
+IF EXIST cs.exe DEL /F cs.exe
+
+echo WRITE C# FILE 
+rem write cs file
+for /f "tokens=*" %%a in (cs.txt) do (
+  echo %%a
+  rem set /a N+=1 REMOVED
+  rem echo %%a >> cs.txt REMOVED
+)
+TIMEOUT 1
+echo [92m-- WRITE COMPLETE[0m
+
+
+echo COMPILE .EXE 
+rem compile .exe file
+cmd /C csc /target:exe cs.txt
+rem error handling
+if ERRORLEVEL 1 (
+  echo [91mERROR COMPILING CS.EXE; REVIEW CODE IN VS[0m
+  PAUSE
+  CLS
+  EXIT
+)
+else (echo [92m-- COMPLETE[0m)
+TIMEOUT 2
+
+
+echo RUN .EXE
+rem run .exe
+start cs.exe
+rem error handling
+if ERRORLEVEL 1 (
+  echo [91mERROR RUNNING CS.EXE; REVIEW CODE IN VS[0m
+  PAUSE
+  CLS
+  EXIT
+)
+else (echo [92m-- COMPLETE[0m)
+PAUSE
+
+
+CLS
+EXIT
+
